4b7e8dcacef56f421a14cce6712f671b1cfb0459
LeonardChang <flydard@163.com>
2013-05-14 16:27:22 +0800

    NGUI tween

---
+++ Client/Assets/NGUI/Scripts/Tweening/TweenPositionEx.cs
@@ -0,0 +1,48 @@
+﻿//----------------------------------------------
+//            NGUI: Next-Gen UI kit
+// Copyright © 2011-2012 Tasharen Entertainment
+//----------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Tween the object's position.
+/// </summary>
+
+[AddComponentMenu("NGUI/Tween/PositionEx")]
+public class TweenPositionEx : UITweener
+{
+	public Vector3 from;
+    public Vector3 mid;
+	public Vector3 to;
+
+    public float midPosition = 0.5f;
+
+	Transform mTrans;
+
+	public Transform cachedTransform { get { if (mTrans == null) mTrans = transform; return mTrans; } }
+	public Vector3 position { get { return cachedTransform.localPosition; } set { cachedTransform.localPosition = value; } }
+
+	override protected void OnUpdate (float factor)
+    {
+        Vector3 old = factor <= midPosition ? from : mid;
+        Vector3 target = factor <= midPosition ? mid : to;
+        float realfactor = factor <= midPosition ? factor / midPosition : (factor - midPosition) / (1 - midPosition);
+
+        cachedTransform.localPosition = old * (1f - realfactor) + target * realfactor;
+    }
+
+	/// <summary>
+	/// Start the tweening operation.
+	/// </summary>
+
+    static public TweenPositionEx Begin(GameObject go, float duration, Vector3 mid, Vector3 pos, float midPosition)
+	{
+        TweenPositionEx comp = UITweener.Begin<TweenPositionEx>(go, duration);
+		comp.from = comp.position;
+        comp.mid = mid;
+		comp.to = pos;
+        comp.midPosition = midPosition;
+		return comp;
+	}
+}
---
+++ Client/Assets/NGUI/Scripts/Tweening/TweenScaleEx.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Tween the object's local scale.
+/// </summary>
+
+[AddComponentMenu("NGUI/Tween/ScaleEx")]
+public class TweenScaleEx : UITweener
+{
+    public Vector3 from;
+    public Vector3 mid;
+    public Vector3 to;
+    public bool updateTable = false;
+
+    public float midPosition = 0.5f;
+
+    Transform mTrans;
+    UITable mTable;
+
+    public Vector3 scale { get { return mTrans.localScale; } set { mTrans.localScale = value; } }
+
+    void Awake()
+    {
+        mTrans = transform;
+        if (updateTable) mTable = NGUITools.FindInParents<UITable>(gameObject);
+    }
+
+    override protected void OnUpdate(float factor)
+    {
+        Vector3 old = factor <= midPosition ? from : mid;
+        Vector3 target = factor <= midPosition ? mid : to;
+        float realfactor = factor <= midPosition ? factor / midPosition : (factor - midPosition) / (1 - midPosition);
+
+        mTrans.localScale = old * (1f - realfactor) + target * realfactor;
+        if (mTable != null) mTable.repositionNow = true;
+    }
+
+    /// <summary>
+    /// Start the tweening operation.
+    /// </summary>
+
+    static public TweenScaleEx Begin(GameObject go, float duration, Vector3 scaleMid, Vector3 scaleEnd, float mid)
+    {
+        TweenScaleEx comp = UITweener.Begin<TweenScaleEx>(go, duration);
+        comp.from = comp.scale;
+        comp.mid = scaleMid;
+        comp.to = scaleEnd;
+        comp.midPosition = mid;
+        return comp;
+    }
+}
